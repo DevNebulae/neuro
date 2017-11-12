dbc70ecf970aade3395382efcb12c9f1867ceb20
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-12 23:53:17 +0100

    Add initial implementation of Neuron class

---
+++ Neuron.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace Neuro
+{
+    public class Neuron
+    {
+        /// <summary>
+        /// A neural net is built up from left to right
+        /// where a neuron is connected with its 
+        /// predecessors and its successors.
+        /// </summary>
+        private NetConnection[] connections;
+        /// <summary>
+        /// The predecessors of this neuron which are
+        /// connected to this neuron. When the neuron is in
+        /// the first layer, the array is null.
+        /// </summary>
+        private Neuron[] previousLayers;
+        /// <summary>
+        /// The successors of this neuron which this neuron
+        /// is connected to. When the neuron is in the last
+        /// layer, the array is null.
+        /// </summary>
+        private Neuron[] nextLayers;
+        private double gradient;
+        private int index;
+
+        /// <summary>
+        /// The ETA is the rate at which the neural net 
+        /// learns, whereby 0 indicates a very slow learner,
+        /// 0.2 indicates a moderate learner and 1.0
+        /// indicates a reckless learner.
+        /// </summary>
+        private static double ETA = 0.15;
+        /// <summary>
+        /// The alpha determines the momentum of the neural
+        /// net where 0 indicates no momentum and 1
+        /// indicates the upper limit in momentum.
+        /// </summary>
+        private static double ALPHA = 0.5;
+
+        public Neuron(Neuron[] previousLayers, Neuron[] nextLayers, int index, Random random)
+        {
+            this.previousLayers = previousLayers;
+            this.nextLayers = nextLayers;
+            this.index = index;
+
+            // Connect layer (n) with layer (n+1) if this
+            // neuron is not in the last layer.
+            if (nextLayers != null)
+            {
+                this.connections = new NetConnection[nextLayers.Length];
+
+                // When this node is not the last node, add
+                // all nodes as a connection with a
+                // randomized weight.
+                for (int i = 0; i < nextLayers.Length; i++)
+                {
+                    this.connections[i].Weight = random.NextDouble();
+                    this.connections[i].DeltaWeight = 0;
+                }
+            }
+            else
+            {
+                this.connections = new NetConnection[0];
+            }
+        }
+    }
+}
