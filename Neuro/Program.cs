2b5a05da792ea8c194678107632ab9d9a9387c58
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-14 19:56:54 +0100

    Move source code to separate project folder

---
+++ Neuro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neuro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
