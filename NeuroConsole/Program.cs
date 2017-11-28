4979b6b632de626f4115ef16503579aaced4388c
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-28 11:37:25 +0100

    Add Program class for NeuroConsole project

---
+++ NeuroConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace NeuroConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] labels = MNISTReader.ReadMNISTLabels(10000, @"assets/t10k-labels.idx1-ubyte");
+            byte[][][] images = MNISTReader.ReadMNISTImages(10000, 28, @"assets/t10k-images.idx3-ubyte");
+
+            for (int i = 0; i < images.Length; i++) {
+                var digitImage = new DigitImage(images[i], labels[i]);
+                Console.WriteLine(digitImage.ToString());
+            }
+        }
+    } 
+}
