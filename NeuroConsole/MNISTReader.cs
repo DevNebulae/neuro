39f9ba3a2d2f48ab1d8dac7c28be09565059b894
DevNebulae <gamerkeeperson+github@gmail.com>
2017-11-28 11:36:47 +0100

    Add MNISTReader class

---
+++ NeuroConsole/MNISTReader.cs
@@ -0,0 +1,97 @@
+using System;
+using System.IO;
+
+namespace NeuroConsole {
+    public class MNISTReader {
+        /// <summary>
+        /// Read an MNIST data file at the specified
+        /// location.
+        /// </summary>
+        /// <param name="size">
+        /// The size of the MNIST set.
+        /// </param>
+        /// <param name="labelLocation">
+        /// The location of the MNIST file.
+        /// </param>
+        /// <returns>
+        /// an array of bytes containing the numbers which
+        /// the written images have been classified as.
+        /// </returns>
+        public static byte[] ReadMNISTLabels(int size, string labelLocation)
+        {
+            byte[] labels = null;
+            FileStream fs = null;
+            
+            try
+            {
+                labels = new byte[size];
+                fs = new FileStream(labelLocation, FileMode.Open);
+                using (BinaryReader labelBinaryReader = new BinaryReader(fs))
+                {
+                    // Read a padding byte
+                    labelBinaryReader.ReadInt32(); 
+                    int numLabels = labelBinaryReader.ReadInt32();
+
+                    for (int i = 0; i < size; i++)
+                        labels[i] = labelBinaryReader.ReadByte();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                fs.Dispose();
+            }
+            
+            return labels;
+        }
+
+        public static byte[][][] ReadMNISTImages(int size, int imageSize, string imageLocation)
+        {
+            // Initialize the array of images with an array
+            // of images[size][imageSize][imageSize]
+            byte[][][] images = new byte[size][][];
+            for (int i = 0; i < images.Length; i++) {
+                images[i] = new byte[imageSize][];
+
+                for (int j = 0; j < images[i].Length; j++)
+                    images[i][j] = new byte[imageSize];
+            }
+
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(imageLocation, FileMode.Open); 
+                using (BinaryReader imageBinaryReader = new BinaryReader(fs))
+                {
+                    // Read a padding byte
+                    imageBinaryReader.ReadInt32();
+                    int numImages = imageBinaryReader.ReadInt32(); 
+                    int numRows = imageBinaryReader.ReadInt32(); 
+                    int numCols = imageBinaryReader.ReadInt32();
+
+                    for (int s = 0; s < size; s++)
+                        for (int i = 0; i < imageSize; i++)
+                            for (int j = 0; j < imageSize; j++) {
+                                byte b = imageBinaryReader.ReadByte();
+                                images[s][i][j] = b;
+                            }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                fs.Dispose();
+            }
+
+            return images;
+        }
+    }
+}
